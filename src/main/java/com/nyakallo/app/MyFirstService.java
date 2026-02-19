ed21de855e8bcfc638ff563af8e109a78c3ea6f0
Nyakallo Mahlakametsa <142156744+Nyakallo1996@users.noreply.github.com>
2026-02-19 12:23:37 +0200

    my spring boot

--- src/main/java/com/nyakallo/app/MyFirstService.java
+++ src/main/java/com/nyakallo/app/MyFirstService.java
@@ -8,11 +8,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class MyFirstService {
 
-    @Autowired
+
     private  MyFirstClass myFirstClass;
 
-
-
+    @Autowired
+    public void SetMyFirstClass(@Qualifier("myFirstBean") MyFirstClass myFirstClass){
+        this.myFirstClass = myFirstClass;
+    }
 
     public String tellAStory(){
         return "the dependency is saying : " + myFirstClass.sayHello();
